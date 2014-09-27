22abfe547c3758a06273e68112ca73708b2da63a
Michael Villar <me@michaelvillar.com>
2014-09-26 18:00:48 -0700

    Fixed http server

--- web.js
+++ web.js
@@ -1,24 +1,5 @@
-var http = require('http');
-var fs = require('fs');
-var connect = require('connect');
-
-var files = ["/index.html", "/css/index.css"];
-
-var sendFile = function(filename, res) {
-  fs.readFile(filename, "binary", function(err, file) {
-    res.writeHead(200);
-    res.write(file, "binary");
-    res.end();
-  });
-};
-
-var app = connect();
-app.use(function (req, res) {
-  if(fs.existsSync("./public/" + req.url))
-    sendFile("./public/" + req.url, res);
-  else
-    sendFile("./public/index.html", res);
-});
-
-http.createServer(app).listen(8000);
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.listen(8000);
 console.info("Listening on port 8000")
