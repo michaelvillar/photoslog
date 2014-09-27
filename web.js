a5a5c404bf5bf113dc29f8ba108950218af9adc5
Michael Villar <me@michaelvillar.com>
2014-09-26 18:05:38 -0700

    Added * route

--- web.js
+++ web.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html', {}, function (err) {
+    if (err) {
+      res.status(err.status).end();
+    }
+  });
+})
 app.listen(8000);
 console.info("Listening on port 8000")
